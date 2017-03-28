1b1b242041db491d3b6bdd9f07154e3b53da1b22
Nam Nguyen <namn1@student.unimelb.edu.au>
2017-03-29 00:56:58 +1100

    almost done with message page

--- fitfamoz/fitfamoz/settings.py
+++ fitfamoz/fitfamoz/settings.py
@@ -124,8 +124,10 @@ STATIC_URL = '/static/'
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
 MEDIA_ROOT = u'/home/fitfamoz/fitfamoz/media'
 MEDIA_URL = '/media/'
+
+STATIC_ROOT = u'/home/fitfamoz/fitfamoz/static'
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static/'),
 )
-STATIC_ROOT = u'/home/fitfamoz/fitfamoz/static'
-STATIC_URL = '/static/'

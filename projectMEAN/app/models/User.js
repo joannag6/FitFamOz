3007ebe3ef3076df173b87bbdbfc3c1efedec449
Nam Nguyen <namn1@student.unimelb.edu.au>
2017-04-19 13:20:07 +1000

    mean stack application added

---
+++ projectMEAN/app/models/User.js
@@ -0,0 +1,21 @@
+/**
+ * Created by NamNguyen1 on 15/4/17.
+ */
+
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+    userID : String,
+    firstName : String,
+    lastName : String,
+    email : String,
+    passWord : String,
+    doB : Date,
+    location : String,
+    activities : [String],
+    friends : [String]
+});
+
+// define our user model
+// module.exports allows us to pass this to other files when it is called
+module.exports = mongoose.model('User', userSchema);
---
+++ projectMEAN/app/routes.js
@@ -0,0 +1,44 @@
+var User = require('./models/User');
+var Conversation = require('./models/Conversation');
+
+var controller = require('../controllers/controller.js');
+
+module.exports = function(app) {
+
+    // server routes ===========================================================
+    // handle things like api calls
+    // authentication routes
+
+    //api routes
+    app.get('/api', controller.findAllUsers);
+
+    app.get('/api/:id', controller.findOneUser);
+
+    app.post('/api', controller.createUser);
+
+    // app.delete('/api/:id', controller.deleleUser());
+
+    app.delete('/api/:id', function(req,res) {
+        var UserInx = req.params.id;
+        User.findByIdAndRemove(UserInx, function (err, user) {
+            if (!err) {
+                res.json({message: 'Successfully deleted', id: user.userID});
+            } else {
+                res.sendStatus(404);
+            }
+        })
+    });
+
+    // frontend routes =========================================================
+
+    // use res.render to load up an ejs view file
+
+    // about page
+    app.get('/about', function(req, res) {
+        res.render('./pages/about');
+    });
+
+    // route to handle all angular requests
+    app.get('/', controller.loadIndex);
+
+};

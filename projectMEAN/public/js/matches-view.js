1fd9b6132551963bf952830384a3c6af524e7766
Qing <yapqingyi@gmail.com>
2017-04-26 14:57:47 +1000

    not needed

--- projectMEAN/public/js/matches-view.js
+++
@@ -1,7 +0,0 @@
-var myApp = angular.module('myApp', []);
-myApp.controller('ViewCtrl', function($scope) {
-  $scope.showMe = false;
-  $scope.viewProfile = function () {
-    $scope.showMe = !$scope.showMe;
-  }
-}

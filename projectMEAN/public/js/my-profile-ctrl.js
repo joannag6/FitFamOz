b1dbe86016dad9e8a1b520122bbc0197bc5a4c8c
Joanna <joannag6@gmail.com>
2017-04-27 11:40:41 +1000

    .

--- projectMEAN/public/js/my-profile-ctrl.js
+++ projectMEAN/public/js/my-profile-ctrl.js
@@ -3,7 +3,16 @@ var myApp = angular.module("myApp");
 myApp.controller("MyProfileCtrl", ["$scope", "$http", function($scope, $http) {
   $scope.editMode = false;
 
-  $scope.myActivities = ["Swimming", "Dog Walking", "Jogging", "Weight Lifting"];
+  $scope.activityLevels = ["High", "Low", "Medium"];
+
+  $scope.myActivities = [
+    {"name": "Swimming", "level": "High"},
+    {"name": "Dog Walking", "level": "Low"},
+    {"name": "Jogging", "level": "High"},
+    {"name": "Weight Lifting", "level": "Medium"}
+  ];
+
+  $scope.thisUser = {"firstName": "John", "lastName": "Smith", "aboutMe": "Hi, I'm looking for a cool gym partner."};
 
   $scope.toggleEditMode = function() {
     $scope.editMode = !$scope.editMode;

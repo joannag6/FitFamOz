c462b5dc5a0c7188cf5c4d8a61e95fdb9f01cf50
Joanna <joannag6@gmail.com>
2017-04-27 13:22:45 +1000

    Included libraries for calendar.

--- projectMEAN/public/js/resource.js
+++ projectMEAN/public/js/resource.js
@@ -3,10 +3,10 @@ var myApp = angular.module("myApp");
 myApp.factory("User", ["$resource", "$http", function($resource, $http) {
   return $resource("/api/:id", { id: "@_id" },
     {
-      'create':  { method: 'POST' },
-      'showAll':    { method: 'GET', isArray: true },
+      'create': { method: 'POST' },
+      'showAll': { method: 'GET', isArray: true },
       'showOne': { method: 'GET', isArray: false },
-      'update':  { method: 'PUT' },
+      'update': { method: 'PUT' },
       'delete': { method: 'DELETE' }
     }
   );

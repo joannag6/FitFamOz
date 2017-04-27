12313a1aa4ec1c6d47ef5c5d92b89a5ce78622bf
Joanna <joannag6@gmail.com>
2017-04-27 10:52:11 +1000

    Added activity skill level to own profile.

--- projectMEAN/public/js/my-profile-ctrl.js
+++ projectMEAN/public/js/my-profile-ctrl.js
@@ -3,7 +3,14 @@ var myApp = angular.module("myApp");
 myApp.controller("MyProfileCtrl", ["$scope", "$http", function($scope, $http) {
   $scope.editMode = false;
 
-  $scope.myActivities = ["Swimming", "Dog Walking", "Jogging", "Weight Lifting"];
+  $scope.activityLevels = ["High", "Low", "Medium"];
+
+  $scope.myActivities = [
+    {"name": "Swimming", "level": "High"},
+    {"name": "Dog Walking", "level": "Low"},
+    {"name": "Jogging", "level": "High"},
+    {"name": "Weight Lifting", "level": "Medium"}
+  ];
 
   $scope.toggleEditMode = function() {
     $scope.editMode = !$scope.editMode;

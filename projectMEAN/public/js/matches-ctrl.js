ee1b1f8a6469c76c9f66f6ca9d8756b140b5dc5c
Joanna <joannag6@gmail.com>
2017-04-28 15:12:06 +1000

    Matches now dynamically shows all users. (no limit set yet.)

--- projectMEAN/public/js/matches-ctrl.js
+++ projectMEAN/public/js/matches-ctrl.js
@@ -1,6 +1,15 @@
 var myApp = angular.module("myApp");
 
-myApp.controller("MatchesCtrl", ["$scope", function($scope) {
-  $scope.users = [1,2,3,4,5,6];
+myApp.controller("MatchesCtrl", ["$scope", "User", function($scope, User) {
+  // $scope.users = [1,2,3,4,5,6];
+  $scope.users = User.query();
+  // .then(
+  //   function(data) {
+  //     $scope.users = data;
+  //     console.log(data);
+  //   }, function(err) {
+  //     console.log(err);
+  // });
+
   $scope.friends = [1,2,3,4,5,6];
 }]);

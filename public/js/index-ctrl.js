6a6284faa9d9312d65cb7dff79e3219493c0f297
Joanna <joannag6@gmail.com>
2017-05-08 18:17:40 +1000

    Added FB Log in, need to integrate with current sign up form.

--- public/js/index-ctrl.js
+++ public/js/index-ctrl.js
@@ -1,6 +1,69 @@
 var myApp = angular.module("myApp");
 
 myApp.controller("IndexCtrl", ["$scope", "User", function($scope, User) {
+
+  // This is called with the results from from FB.getLoginStatus().
+  $scope.statusChangeCallback = function(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      $scope.testAPI();
+    } else {
+      // The person is not logged into your app or we are unable to tell.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    }
+  }
+
+  // This function is called when someone finishes with the Login
+  // Button.  See the onlogin handler attached to it in the sample
+  // code below.
+  $scope.checkLoginState = function() {
+    FB.getLoginStatus(function(response) {
+      $scope.statusChangeCallback(response);
+    });
+  }
+
+  window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '1249707755145859',
+    cookie     : true,  // enable cookies to allow the server to access
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.8' // use graph api version 2.8
+  });
+
+  FB.getLoginStatus(function(response) {
+    $scope.statusChangeCallback(response);
+  });
+
+  };
+
+  // Load the SDK asynchronously
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  // Here we run a very simple test of the Graph API after login is
+  // successful.  See statusChangeCallback() for when this call is made.
+  $scope.testAPI = function() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', {fields: ['first_name', 'last_name', 'email']}, function(response) {
+      console.log('Successful login for: ' + response.given_name);
+      console.log(response);
+    });
+  }
+
+
   $scope.currPage = "index";
 
   $scope.activities = [

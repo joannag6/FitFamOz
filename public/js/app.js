05ecc4db2a40797215ed72e7761bd4b4d179fc7d
Nelson Chen <nelsonchen96@gmail.com>
2017-05-15 23:47:05 +1000

    Google login needs permission verification

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,7 @@
 var myApp = angular.module("myApp", ["ngResource", "ngStorage", "ui.toggle"]);
+
+// Google Login
+function onLoadFunction(){
+  gapi.client.setApiKey('AIzaSyBzISd-U2gq5UlNqfI6Jf4Y42YK2lC-c3E');
+  gapi.client.load('plus', 'v1', function(){});
+}
--- public/js/navbar-ctrl.js
+++ public/js/navbar-ctrl.js
@@ -2,6 +2,40 @@ var myApp = angular.module("myApp");
 
 myApp.controller("NavbarCtrl", ["$scope", "User", function($scope, User) {
 
+  /****************************HANDLE GOOGLE LOGIN***********************/
+  $scope.gmail = {
+    username: "",
+    email: ""
+  };
+
+  $scope.onGoogleLogin = function(){
+    var params = {
+      'clientid': '1003741852857-k6metff6h6978bmjked4od77cdc577bc.apps.googleusercontent.com',
+      'cookiepolicy': 'single_host_origin',
+      'callback': function(result) {
+        if (result['status']['signed_in']) {
+          var request = gapi.client.plus.people.get(
+            {
+              'userId': 'me'
+            }
+          );
+          request.execute(function(resp) {
+            $scope.$apply(function() {
+              $scope.gmail.username = resp.displayName;
+              $scope.gmail.email = resp.emails[0].value;
+              //$sceop.g_image = resp.image.url;
+            })
+          })
+        }
+      },
+      'approvalprompt': 'force',
+      'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read'
+    };
+
+    gapi.auth.signIn(params);
+  }
+
+  /**********************************************************************/
   // This is called with the results from from FB.getLoginStatus().
   $scope.statusChangeCallback = function(response) {
     console.log(response);
